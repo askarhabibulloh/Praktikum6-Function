19e090d0d475f661fa71776ddcc91f12c4407d4b
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2023-10-13 07:34:21 +0700

    13 Oktober 2023

---
+++ Built_in_function/String_Function/05.trim.php
@@ -0,0 +1,8 @@
+<?php
+$username = "admin ";
+if($username=="admin"){
+    echo"Selamat Datang admin";
+}else{
+    echo"Anda bukan admin";
+}
+?>
---
+++ Built_in_function/String_Function/06.trim.php
@@ -0,0 +1,8 @@
+<?php
+$username = "admin ";
+if(trim($username)=="admin"){
+    echo"Selamat Datang admin";
+}else{
+    echo"Anda bukan admin";
+}
+?>
